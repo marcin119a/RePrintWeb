919fa8123d42b3b443688b3254692e1a9079a7f4
Marcin Wierzbiński <marcin119a@gmail.com>
2024-06-03 17:55:36 +0200

    load button + inital load

--- pages/page2.py
+++ pages/page2.py
@@ -1,7 +1,7 @@
 from utils.figpanel import create_main_dashboard
 from dash import dcc, html
 from main import app
-from dash import Input, Output
+from dash import Input, Output, State
 import dash_bootstrap_components as dbc
 from pages.nav import navbar
 import pandas as pd
@@ -25,6 +25,7 @@ dropdown_options = [{'label': file, 'value': file} for file in files]
 # Application layout
 page2_layout = html.Div([
     navbar,
+    dcc.Interval(id='initial-load', interval=1000, n_intervals=0, max_intervals=1),
     dbc.Container([
     dbc.Row([
         dcc.Dropdown(
@@ -45,6 +46,7 @@ page2_layout = html.Div([
                 value=[k for k in data['COSMIC_v2_SBS_GRCh37.txt']],
             ),
     ]),
+    dbc.Button("Generate plots", id="reload-button", className="ms-2"),
     dcc.Location(id='url-page2', refresh=False),
     dcc.Loading(
         id="loading-graphs",
@@ -57,44 +59,55 @@ page2_layout = html.Div([
 
 
 import plotly.graph_objects as go
-
+import dash
+from dash.exceptions import PreventUpdate
 @app.callback(
     Output('plots-container-2', 'children'),
-    [Input('signatures-dropdown-2', 'value'),
-     Input('dropdown-2', 'value')]
+    [Input('initial-load', 'n_intervals'),
+     Input('dropdown-2', 'value'),
+     Input('reload-button', 'n_clicks')],
+    [State('signatures-dropdown-2', 'value')]
 )
-def update_graph(selected_signatures, selected_file):
-    if not selected_signatures or not selected_file:
-        return []
+def update_graph(init_load, selected_file, n_clicks, selected_signatures):
+    ctx = dash.callback_context
 
-    df_signatures = pd.read_csv(f"data/signatures/{selected_file}", sep='\t', index_col=0)[selected_signatures]
-    df_reprint = pd.read_csv(f"data/cosmic_reprints/{selected_file}.reprint", sep='\t', index_col=0)[selected_signatures]
+    if not ctx.triggered:
+        trigger_id = 'initial-load'
+    else:
+        trigger_id = ctx.triggered[0]['prop_id'].split('.')[0]
 
-    plots = []
-    for signature in selected_signatures:
-        plots.append(
-            dbc.Row([
-                dbc.Col(
-                    dcc.Graph(
-                        figure=create_main_dashboard(
-                            df_signatures,
-                            signature=signature,
-                            title=f'{signature} Frequency of Specific Tri-nucleotide Context Mutations by Mutation Type'
-                        )
-                    ), width=6
-                ),
-                dbc.Col(
-                    dcc.Graph(
-                        figure=create_main_dashboard(
-                            df_reprint,
-                            signature=signature,
-                            title=f'{signature} Reprint - Frequency of Specific Tri-nucleotide Context Mutations by Mutation Type'
-                        )
-                    ), width=6
-                )
-            ])
-        )
-    return plots
+    if trigger_id == 'initial-load' or trigger_id == 'reload-button':
+        if not selected_signatures or not selected_file:
+            return []
+
+        df_signatures = pd.read_csv(f"data/signatures/{selected_file}", sep='\t', index_col=0)[selected_signatures]
+        df_reprint = pd.read_csv(f"data/cosmic_reprints/{selected_file}.reprint", sep='\t', index_col=0)[selected_signatures]
+
+        plots = []
+        for signature in selected_signatures:
+            plots.append(
+                dbc.Row([
+                    dbc.Col(
+                        dcc.Graph(
+                            figure=create_main_dashboard(
+                                df_signatures,
+                                signature=signature,
+                                title=f'{signature} Frequency of Specific Tri-nucleotide Context Mutations by Mutation Type'
+                            )
+                        ), width=6
+                    ),
+                    dbc.Col(
+                        dcc.Graph(
+                            figure=create_main_dashboard(
+                                df_reprint,
+                                signature=signature,
+                                title=f'{signature} Reprint - Frequency of Specific Tri-nucleotide Context Mutations by Mutation Type'
+                            )
+                        ), width=6
+                    )
+                ])
+            )
+        return plots
 
 @app.callback(
     [Output('signatures-dropdown-2', 'options'),

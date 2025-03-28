22b492b987cf7cd5d9bd1a2f1134004c4b94f48c
Marcin Wierzbiński <marcin119a@gmail.com>
2025-03-28 21:01:39 +0100

    merged signatures

--- app.py
+++ app.py
@@ -53,4 +53,4 @@ def set_active_nav(pathname):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8080)
--- pages/page4.py
+++ pages/page4.py
@@ -59,8 +59,42 @@ page4_layout = html.Div([
             multiple=False
         ),
     html.Div(id='info_uploader-4'),
+    dbc.Alert(
+        [
+            html.H5("Expected File Format", style={"font-size": "18px", "font-weight": "bold"}),
+            html.P(
+                "The uploaded file should be a tab-separated file (.txt) containing mutation types and corresponding mutation signatures.",
+                style={"font-size": "14px"}),
+            html.P("Columns:", style={"font-size": "14px", "margin-bottom": "5px"}),
+            html.Ul([
+                html.Li("Type: Mutation type (e.g., A[C>A]A, A[C>A]C, ...).", style={"font-size": "13px"}),
+                html.Li("SBS1, SBS2, ..., SBSN: Signature mutation values (frequencies or probabilities).",
+                        style={"font-size": "13px"})
+            ], style={"padding-left": "20px", "margin-bottom": "5px"}),
+            html.P("Example first few rows:", style={"font-size": "14px", "margin-bottom": "5px"}),
+            html.Pre(
+                "Type\tSBS1\tSBS2\tSBS3\n"
+                "A[C>A]A\t0.001\t0.002\t0.003\n"
+                "A[C>A]C\t0.004\t0.005\t0.006",
+                style={"white-space": "pre-wrap", "font-family": "monospace", "font-size": "12px",
+                       "background-color": "#f8f9fa", "padding": "5px"}
+            ),
+        ],
+        color="info",
+        dismissable=True,
+        style={"font-size": "14px", "padding": "10px"}),
     dcc.Store(id='session-4-signatures', storage_type='session', data=None),
     dbc.Button("Generate plots", id="reload-button", className="ms-2"),
+    dbc.Row([
+            dbc.Col([
+                html.H5("Signature similarity"),
+                dcc.Graph(id='heatmap-plot-4')
+            ]),
+            dbc.Col([
+                html.H5("RePrint similarity"),
+                dcc.Graph(id='heatmap-reprint-plot-4')
+            ])
+    ]),
     dcc.Location(id='url-page4', refresh=False),
     dcc.Loading(
         id="loading-graphs",
@@ -74,7 +108,9 @@ page4_layout = html.Div([
 import dash
 from utils.utils import reprint
 @app.callback(
-    Output('plots-container-4', 'children'),
+    [Output('plots-container-4', 'children'),
+     Output('heatmap-plot-4', 'figure'),
+     Output('heatmap-reprint-plot-4', 'figure')],
     [Input('initial-load', 'n_intervals'),
      Input('dropdown-4', 'value'),
      Input('reload-button', 'n_clicks')],
@@ -91,11 +127,9 @@ def update_graph(init_load, selected_file, n_clicks, selected_signatures, signat
 
     if trigger_id == 'initial-load' or trigger_id == 'reload-button':
         if not selected_signatures or not selected_file:
-            return []
+            return [], {}, {}
         if signatures is not None:
             df_signatures = pd.DataFrame(signatures['signatures_data'])
-            print(df_signatures.shape)
-            print('test')
             df_signatures.index = df_signatures['Type']
             df_signatures = df_signatures.drop(columns='Type')
             df_reprint = reprint(df_signatures, epsilon=0.0001)[selected_signatures]
@@ -124,13 +158,16 @@ def update_graph(init_load, selected_file, n_clicks, selected_signatures, signat
                                 df_reprint,
                                 signature=signature,
                                 title=f'{signature} Reprint - Probabilities of Specific Tri-nucleotide Context Mutations by Mutation Type',
-                                yaxis_title='Probabilites'
+                                yaxis_title='Probabilities'
                             )
                         ), width=6
                     )
                 ])
             )
-        return plots
+        from utils.figpanel import create_heatmap_with_rmse
+        return plots, create_heatmap_with_rmse(df_signatures),create_heatmap_with_rmse(df_reprint)
+    else:
+        return [], {}, {}
 
 @app.callback(
     [Output('session-4-signatures', 'data')],
@@ -154,9 +191,7 @@ def update_output_signatures(contents, filename, existing_data):
             # Połączenie starych i nowych danych, bez duplikatów
             merged_df = pd.merge(existing_df, new_df_signatures, on='Type', how='inner')
             merged_df.index = merged_df['Type']
-            print(merged_df.to_csv('test.csv'))
         else:
-            print('tutaj jest warunek')
             merged_df = new_df_signatures
 
         # Konwersja do listy rekordów dla sesji
@@ -171,29 +206,29 @@ def update_output_signatures(contents, filename, existing_data):
      Output('signatures-dropdown-4', 'value'),
      Output('dropdown-4', 'style'),
      Output('info_uploader-4', 'children')],
-    [Input('dropdown-4', 'value'),
-     Input('session-4-signatures', 'data')]
+    [Input('session-4-signatures', 'data')]
 )
-def set_options(selected_category, contents):
-    # Jeśli użytkownik załadował nowy plik, dodajemy jego zawartość do dropdowna
+def set_options(contents):
     if contents is not None:
         df = pd.DataFrame(contents['signatures_data'])
-        
+
         if 'Type' in df.columns:
             df.index = df['Type']
             df = df.drop(columns='Type')
 
+        # Pobieramy listę sygnatur z załadowanych danych
         uploaded_signatures = df.columns.to_list()
 
+        # Łączymy oba zestawy sygnatur, unikając duplikatów
+        all_signatures = list(set(uploaded_signatures))
 
         return (
-            [{'label': signature, 'value': signature} for signature in uploaded_signatures],
-            uploaded_signatures,  # Domyślnie zaznaczamy wszystkie
+            [{'label': signature, 'value': signature} for signature in all_signatures],
+            all_signatures,
             {'display': 'None'},
             f'Added your signatures from {contents["filename"]}'
         )
 
-    # Jeśli nic nie załadowano, pozostawiamy dropdown z oryginalnymi danymi
     return (
         [{'label': f"{i}", 'value': i} for i in data[selected_category]],
         [i for i in data[selected_category]],
